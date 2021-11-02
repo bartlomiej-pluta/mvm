d1c3b8a3cba58483a9d6746b5139448b7381e942
Bartłomiej Przemysław Pluta <noreply.git@mx.bartlomiejpluta.com>
2021-11-02 16:15:12 +0100

    Model virtual machine ADT

---
+++ app/VirtualMachine.hs
@@ -0,0 +1,20 @@
+module VirtualMachine (
+  VM(..),
+  empty
+)
+where
+
+import qualified Data.Sequence as S
+
+data VM = VM { pc :: Int
+             , fp :: Int
+             , stack :: S.Seq Int
+             , halt :: Bool
+             } deriving (Show, Eq)
+
+empty :: VM
+empty = VM { pc     = -1
+           , fp     = -1
+           , stack  = S.empty
+           , halt   = False
+           }
