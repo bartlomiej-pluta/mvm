f9496adf8c15640a15f6c2877267f6d577438480
Bartłomiej Przemysław Pluta <noreply.git@mx.bartlomiejpluta.com>
2021-11-08 16:42:28 +0100

    Create compiler facade for Tokenizer, Parser and Emitter

---
+++ app/Assembler/Compiler.hs
@@ -0,0 +1,10 @@
+module Assembler.Compiler where
+
+import Data.Word (Word8)
+
+import Assembler.Tokenizer (tokenize)
+import Assembler.Parser (parse)
+import Assembler.Emitter (emit)
+
+compile :: String -> Either String [Word8]
+compile input = return input >>= tokenize >>= parse >>= emit
--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
+import System.Environment
+import qualified Data.ByteString as B
+import qualified VirtualMachine as VM
+
+import Assembler.Compiler (compile)
+
+
+run :: String -> IO ()
+run input = case compile input of
+  (Right bytes) -> print $ VM.run VM.empty (B.pack bytes)
+  (Left err) -> putStrLn err
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  (filename:_) <- getArgs
+  input <- readFile filename
+  run input
